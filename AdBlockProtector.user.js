d7e9a0dbd5c2a24ec9d95389ee564a81615183a2
X01X012013 <1010535820@qq.com>
2016-07-15 13:26:51 -0600

    update library

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -11,15 +11,36 @@
 // @downloadURL https://x01x012013.github.io/AdBlockProtector/AdBlockProtector.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-	
-    var _eval = unsafeWindow.eval;
-    unsafeWindow.eval = function(str){
-        if (str.match("blockadblock")) {
-            console.error("eval() says: Refuse to execute: BlockAdBlock detected. ");
-        } else {
-            return _eval(str);
-        }
+    debugMode = true;
+    //=====Common Functions=====
+    //ActivateEval Filter: prevent string with specific keywords from executing
+    //@param filter (RegExp): Filter used to test string before it goes into eval()
+    //@param [optional default=false] doThrow (boolean): If true, eval() will throw when the string did not pass the test
+    const activateEvalFilter = function (filter, doThrow) {
+        const errMsg = "Uncaught AdBlock Error: AdBlocker detectors are not allowed. ";
+        let _eval = unsafeWindow.eval;
+        unsafeWindow.eval = function (str) {
+            //Debug mode
+            if (debugMode) {
+                console.info("eval() just received a call: ");
+                console.log(str);
+            }
+            //Check if the string is allowed
+            if (filter.test(str)) {
+                //Not allowed
+                if (doThrow) {
+                    throw errMsg
+                } else {
+                    return console.error(errMsg);
+                }
+            } else {
+                //Allowed
+                return _eval(str);
+            }
+        };
     };
+    //=====Rules=====
+    activateEvalFilter(/blockadblock/i);
 })();
