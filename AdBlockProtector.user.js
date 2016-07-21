829ef4b3408999541e4b6aea222df2c9b0f67c54
X01X012013 <1010535820@qq.com>
2016-07-21 10:00:45 -0600

    1.0.47

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -2,7 +2,7 @@
 // @name AdBlock Protector
 // @description Temporary solutions against AdBlock detectors
 // @author X01X012013
-// @version 1.0.46
+// @version 1.0.47
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
