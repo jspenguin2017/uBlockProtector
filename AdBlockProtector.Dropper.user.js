2527475d5c8bef0c49864505905c91a87a46b073
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-01-29 07:57:42 -0700

    dropper 1.0.1

--- AdBlockProtector.Dropper.user.js
+++ AdBlockProtector.Dropper.user.js
@@ -2,27 +2,26 @@
 // @name AdBlock Protector Script Dropper
 // @description This is a dropper as a workaround for Greasemonkey
 // @author X01X012013
-// @version 1.0.0
+// @version 1.0.1
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
-// @grant GM_xmlhttpRequest
+// @grant none
 // @run-at document-start
 // @homepage https://x01x012013.github.io/AdBlockProtector/
 // @supportURL https://github.com/X01X012013/AdBlockProtector/issues
 // @downloadURL https://x01x012013.github.io/AdBlockProtector/AdBlockProtector.Dropper.user.js
 // ==/UserScript==
 
-(function () {
-    "use strict";
-    //Set unsafeWindow
-    window.eval("const unsafeWindow = window;");
-    //Drop script
-    GM_xmlhttpRequest({
-        method: "GET",
-        url: "https://x01x012013.github.io/AdBlockProtector/AdBlockProtector.GM_Dropper.user.js",
-        onload: function (response) {
-            window.eval(response.responseText);
-        }
-    });
-})();
+//Set variable
+var unsafeWindow = window;
+//Load script
+var request = new XMLHttpRequest();
+request.open("GET", "https://x01x012013.github.io/AdBlockProtector/AdBlockProtector.user.js", false);
+request.send(null);
+//Check load state and drop script
+if (request.status === 200) {
+    eval(request.responseText);
+} else {
+    alert("Could not load AdBlock Protector. ");
+}
