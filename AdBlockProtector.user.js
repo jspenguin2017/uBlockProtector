e6cd928950a8f8fabf034eeb063a5c42e2eb8245
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-02-07 16:01:17 -0700

    This is a milestone of this project. Thank you to everyone who supported this project for the past 7 month, and together, we will make it better. We are updating the license so we can use code from other source. Archive will be removed because Git history renders them useless. Old versions that are found in Git history will still be under the old license (can be found here: https://x01x012013.github.io/Global/license.html ) The new license starts to take effect starting version 4.

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -2,7 +2,7 @@
 // @name AdBlock Protector Script
 // @description Quick solutions against AdBlock detectors
 // @author X01X012013
-// @version 3.0.21
+// @version 4.0.0
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
