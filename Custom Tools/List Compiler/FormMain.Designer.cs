dbc4b4c749cb5658511b7cd7f2246f0ed7b96e9f
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-02-11 10:05:46 -0700

    list compiler

---
+++ Custom Tools/List Compiler/FormMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace List_Compiler
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelGitRoot = new System.Windows.Forms.Label();
+            this.TBGitRoot = new System.Windows.Forms.TextBox();
+            this.BtnGo = new System.Windows.Forms.Button();
+            this.TBLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // LabelGitRoot
+            // 
+            this.LabelGitRoot.AutoSize = true;
+            this.LabelGitRoot.Location = new System.Drawing.Point(7, 8);
+            this.LabelGitRoot.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LabelGitRoot.Name = "LabelGitRoot";
+            this.LabelGitRoot.Size = new System.Drawing.Size(63, 14);
+            this.LabelGitRoot.TabIndex = 0;
+            this.LabelGitRoot.Text = "Git Root";
+            // 
+            // TBGitRoot
+            // 
+            this.TBGitRoot.Location = new System.Drawing.Point(73, 7);
+            this.TBGitRoot.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TBGitRoot.Name = "TBGitRoot";
+            this.TBGitRoot.Size = new System.Drawing.Size(389, 23);
+            this.TBGitRoot.TabIndex = 1;
+            // 
+            // BtnGo
+            // 
+            this.BtnGo.Location = new System.Drawing.Point(10, 36);
+            this.BtnGo.Name = "BtnGo";
+            this.BtnGo.Size = new System.Drawing.Size(451, 39);
+            this.BtnGo.TabIndex = 2;
+            this.BtnGo.Text = "Go";
+            this.BtnGo.UseVisualStyleBackColor = true;
+            this.BtnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // TBLog
+            // 
+            this.TBLog.Location = new System.Drawing.Point(10, 81);
+            this.TBLog.Name = "TBLog";
+            this.TBLog.Size = new System.Drawing.Size(452, 348);
+            this.TBLog.TabIndex = 3;
+            this.TBLog.Text = "";
+            this.TBLog.WordWrap = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 441);
+            this.Controls.Add(this.TBLog);
+            this.Controls.Add(this.BtnGo);
+            this.Controls.Add(this.TBGitRoot);
+            this.Controls.Add(this.LabelGitRoot);
+            this.Font = new System.Drawing.Font("宋体", 10F);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormMain";
+            this.Text = "List Compiler";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelGitRoot;
+        private System.Windows.Forms.TextBox TBGitRoot;
+        private System.Windows.Forms.Button BtnGo;
+        private System.Windows.Forms.RichTextBox TBLog;
+    }
+}
+
---
+++ Custom Tools/List Compiler/FormMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_Compiler
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Default git root directory
+        /// </summary>
+        private const string DefaultGitRoot = "C:\\Data\\Git\\AdBlockProtector";
+
+        /// <summary>
+        /// Form load event handler
+        /// Set default git root
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            TBGitRoot.Text = DefaultGitRoot;
+        }
+
+        /// <summary>
+        /// Go button click handler
+        /// Start compiling
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BtnGo_Click(object sender, EventArgs e)
+        {
+            //Lock elements
+            BtnGo.Enabled = false;
+            TBGitRoot.Enabled = false;
+            //Cache git root
+            string gitRoot = TBGitRoot.Text;
+            //Start main process
+            await Task.Run(() =>
+            {
+                //Load to remove list (hard coded file name)
+                string[] rmList;
+                try
+                {
+                    rmList = File.ReadAllLines(Path.Combine(gitRoot, "ListCompiler.ToRemove.txt"));
+                    putLog("To remove list read, " + rmList.Length.ToString() + " entries found. ");
+                }
+                catch (Exception err)
+                {
+                    putLog("Cannot read to remove list! Error message: ");
+                    putLog(err.Message);
+                    return;
+                }
+                //Load AAK List
+                string[] originalAAKList;
+                try
+                {
+                    originalAAKList = File.ReadAllLines(Path.Combine(gitRoot, "anti-adblock-killer\\anti-adblock-killer-filters.txt"));
+                    putLog("Original AAK List read, " + originalAAKList.Length.ToString() + " entries found. ");
+                }
+                catch (Exception err)
+                {
+                    putLog("Cannot read original AAK List! Error message: ");
+                    putLog(err.Message);
+                    return;
+                }
+                //Patch AAK List
+                List<string> patchedAAKList = new List<string>();
+                patchedAAKList.Add("! =====Patched AAK List=====");
+                int commentCounter = 0;
+                int removeCounter = 0;
+                for (int i = 0; i < originalAAKList.Length; i++)
+                {
+                    string t = originalAAKList[i];
+                    if (t[0] == '!')
+                    {
+                        //Skip comments
+                        commentCounter++;
+                    }
+                    else if (rmList.Contains(t))
+                    {
+                        //Skip removed entries
+                        removeCounter++;
+                    }
+                    else
+                    {
+                        //Put into patched list
+                        patchedAAKList.Add(t);
+                    }
+                }
+                putLog("Patching finished, " + commentCounter.ToString() + " comments skipped, " + removeCounter.ToString() + " entries removed. ");
+                //Read main file
+                string[] mainFile;
+                try
+                {
+                    mainFile = File.ReadAllLines(Path.Combine(gitRoot, "ListCompiler.Main.txt"));
+                    putLog("Main file read, " + mainFile.Length.ToString() + " entries found. ");
+                }
+                catch (Exception err)
+                {
+                    putLog("Cannot read main file! Error message: ");
+                    putLog(err.Message);
+                    return;
+                }
+                //Combine entries
+                string[] toWrite = mainFile.Concat(patchedAAKList.ToArray()).ToArray();
+                try
+                {
+                    File.WriteAllLines(Path.Combine(gitRoot, "AdBlockProtectorList.txt"), toWrite);
+                    putLog("Compiling finished, " + toWrite.Length.ToString() + " entries wrote. ");
+                }
+                catch (Exception err)
+                {
+                    putLog("Cannot write to output file! Error message: ");
+                    putLog(err.Message);
+                    return;
+                }
+            });
+            //Unlock elements
+            BtnGo.Enabled = true;
+            TBGitRoot.Enabled = true;
+        }
+
+        /// <summary>
+        /// Write log into log textbox, a new line will be automatically added
+        /// This method can be called from another thread
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        private void putLog(string msg)
+        {
+            if (TBLog.InvokeRequired)
+            {
+                Invoke((MethodInvoker)(() =>
+                {
+                    TBLog.Text += msg + Environment.NewLine;
+                }));
+            }
+            else
+            {
+                TBLog.Text += msg + Environment.NewLine;
+            }
+        }
+    }
+}
