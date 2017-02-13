70339fd3e8edb5c660e6fe4fc59eb39bb75862ae
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-02-13 13:24:33 -0700

    update source

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -2,7 +2,7 @@
 // @name AdBlock Protector Script
 // @description Ultimage solution against AdBlock detectors
 // @author X01X012013
-// @version 5.33
+// @version 5.34
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
@@ -12,7 +12,7 @@
 // @run-at document-start
 // @homepage https://x01x012013.github.io/AdBlockProtector/
 // @supportURL https://github.com/X01X012013/AdBlockProtector/issues
-// @downloadURL https://x01x012013.github.io/AdBlockProtector/AdBlockProtector.user.js
+// @downloadURL https://github.com/X01X012013/AdBlockProtector/raw/gh-pages/AdBlockProtector.user.js
 // @grant GM_addStyle
 // @grant GM_getValue
 // @grant GM_setValue
