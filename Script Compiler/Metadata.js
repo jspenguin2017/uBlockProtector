d1ced66c8335883d4f729cd804128034e2b23aa9
X01X012013 <xuhaiyang_1997@hotmail.com>
2017-02-18 09:24:30 -0700

    update path

--- Script Compiler/Metadata.js
+++ Script Compiler/Metadata.js
@@ -12,7 +12,7 @@
 // @run-at document-start
 // @homepage https://x01x012013.github.io/AdBlockProtector/
 // @supportURL https://github.com/X01X012013/AdBlockProtector/issues
-// @downloadURL https://github.com/X01X012013/AdBlockProtector/raw/gh-pages/AdBlockProtector.user.js
+// @downloadURL https://github.com/X01X012013/AdBlockProtector/raw/master/AdBlockProtector.user.js
 // @require https://github.com/X01X012013/AdBlockProtector/raw/gh-pages/jQuery/Core.Factory.3.1.1.min.js
 // @require https://github.com/X01X012013/AdBlockProtector/raw/gh-pages/jQuery/Color.Factory.2.1.2.min.js
 // @require https://github.com/X01X012013/AdBlockProtector/raw/gh-pages/Script%20Compiler/Core.js
