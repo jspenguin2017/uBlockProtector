b0c08b186e8928c2f3ce6f58ad2b5000283d25c8
X01X012013 <xuhaiyang_1997@hotmail.com>
2017-02-20 17:56:13 -0700

    update build settings

--- List Compiler/List Compiler/Properties/AssemblyInfo.cs
+++ List Compiler/List Compiler/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("List Compiler")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Script Compiler/Script Compiler/Properties/AssemblyInfo.cs
+++ Script Compiler/Script Compiler/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Script Compiler")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
