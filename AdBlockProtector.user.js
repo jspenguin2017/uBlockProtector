b899874f4c4f43c6637fdeed4ba9da23028b64f0
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-02-22 12:16:09 -0700

    old solution: if (a.domCmp(["latimes.com"])) {     //Watch and remove block screen     a.filter("addEventListener", /return\"undefined\"\!\=\=typeof\ e/);     a.css("html,body {overflow:scroll;}");     a.observe("insert", function (node) {         if (node.innerHTML &&             node.innerHTML.includes("please turn off your ad blocker or subscribe")) {             node.remove();         }     }); } new: solved by List

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -2,7 +2,7 @@
 // @name AdBlock Protector Script
 // @description Ultimage solution against AdBlock detectors
 // @author X01X012013
-// @version 6.30
+// @version 6.31
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
@@ -1874,17 +1874,6 @@ if (a.domCmp(["zrabatowani.pl"])) {
 if (a.domCmp(["hanime.tv"])) {
     //NSFW! Lock BetterJsPop to an empty function
     a.readOnly("BetterJsPop", function () { });
-}
-if (a.domCmp(["latimes.com"])) {
-    //Watch and remove block screen
-    a.filter("addEventListener", /return\"undefined\"\!\=\=typeof\ e/);
-    a.css("html,body {overflow:scroll;}");
-    a.observe("insert", function (node) {
-        if (node.innerHTML &&
-            node.innerHTML.includes("please turn off your ad blocker or subscribe")) {
-            node.remove();
-        }
-    });
 }
 //Activate generic protectors, excluded domains check is handled inside
 a.generic();
