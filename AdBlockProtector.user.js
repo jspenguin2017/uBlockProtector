38464781c3185e7ac8c36532859168c356388ca4
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-04-01 08:29:38 -0600

    6.128

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -2,7 +2,7 @@
 // @name AdBlock Protector Script
 // @description Ultimate solution against AdBlock detectors
 // @author X01X012013
-// @version 6.127
+// @version 6.128
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
@@ -910,7 +910,7 @@ if (a.domCmp(["tvregionalna24.pl"])) {
         }
     });
 }
-if (a.domCmp(["tvn.pl", "tvnstyle.pl", "tvnturbo.pl", "kuchniaplus.pl"])) {
+if (a.domCmp(["tvn.pl", "tvn24.pl", "tvnstyle.pl", "tvnturbo.pl", "kuchniaplus.pl"])) {
     const homePages = ["http://www.tvn.pl/", "http://www.tvn7.pl/", "http://www.tvnstyle.pl/",
 "http://www.tvnturbo.pl/"];
     if (!homePages.includes(a.doc.location.href)) {
