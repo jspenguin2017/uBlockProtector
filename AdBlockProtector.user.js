31e2108354b6b57502d49e5cb47b3be40f493b48
x01x012013 <xuhaiyang_1997@hotmail.com>
2017-04-13 09:50:50 -0600

    new build

--- AdBlockProtector.user.js
+++ AdBlockProtector.user.js
@@ -2,7 +2,7 @@
 // @name AdBlock Protector Script
 // @description Ultimate solution against AdBlock detectors
 // @author jspenguin2017
-// @version 6.156
+// @version 6.157
 // @encoding utf-8
 // @include http://*/*
 // @include https://*/*
