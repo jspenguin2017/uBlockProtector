c32137588808fdeb593413c591ecb931086215ff
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-11 21:12:15 -0600

    this should fix CI

--- Tests/syntax.node.js
+++ Tests/syntax.node.js
@@ -7,6 +7,10 @@ console.log("Syntax test started.");
 const esprima = require("esprima");
 const { readFileSync } = require("fs");
 
+//Load the compiled script
+const dist = readFileSync("uBlockProtector.user.js").toString();
+//Print a sample of the file to make sure the file is properly loaded
+console.log("Compiled script loaded, here is a small sample of the file:");
+console.log(dist.substring(1000, 2000));
 //Verify the compiled script
-const dist = readFileSync("uBlockProtector.user.js");
 esprima.parse(dist);
