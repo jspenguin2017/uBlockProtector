25e6c6d9fad1ceff1a16907aac1aee2123a2bfb7
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-23 15:25:59 -0600

    test encrypted variable

--- Continuous Integration/3-build.node.js
+++ Continuous Integration/3-build.node.js
@@ -3,6 +3,11 @@
 
 console.log("=====3-build.node.js starts=====");
 
-//
+//Test secure variable
+if (process.env.SECRET === "test") {
+    console.log("secure environment variable is working properly.");
+} else {
+    process.exit(1);
+}
 
 console.log("=====3-build.node.js ends=====");
