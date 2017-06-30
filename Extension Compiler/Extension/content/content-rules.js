bf1243ee0288533dd2fc91b05212b64f9086200e
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-30 10:11:09 -0600

    https://github.com/jspenguin2017/uBlockProtector/issues/339

--- Extension Compiler/Extension/content/content-rules.js
+++ Extension Compiler/Extension/content/content-rules.js
@@ -2607,7 +2607,7 @@ if (a.domCmp(["cutwin.com"])) {
     a.bait("div", "#test-block", true);
     a.timewarp("setInterval", a.matchMethod.stringExact, "1000");
 }
-if (a.domCmp(["adshort.co", "linksh.top"])) {
+if (a.domCmp(["adshort.co", "linksh.top", "adshorte.com"])) {
     a.bait("div", "#test-block", true);
     a.noAccess("_pop");
     a.noAccess("F3Z9");
