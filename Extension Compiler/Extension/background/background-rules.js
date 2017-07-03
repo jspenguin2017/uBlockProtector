8983e8acaaf08bca8e42301b793f4b74ad481abc
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-07-03 17:18:17 -0600

    update log that sounds weird

--- Extension Compiler/Extension/background/background-rules.js
+++ Extension Compiler/Extension/background/background-rules.js
@@ -12,7 +12,7 @@ a.generic();
         //Event callbacks are blocked by List
         let payload = `(() => {
             "use strict";
-            window.console.warn("uBlock Protector locally loopbacked a request to mmod.v.fwmrm.net");
+            window.console.warn("A request to mmod.v.fwmrm.net is locally loopbacked by uBlock Protector.");
             ${cbfn}({
                 version: "1",
                 networkId: "100",
