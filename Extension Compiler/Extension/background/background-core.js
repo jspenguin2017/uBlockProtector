2beb3ecd101dd5f40c3ca6e1bef192e77e6facdc
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-07-04 13:46:17 -0600

    https://github.com/jspenguin2017/uBlockProtector/issues/345

--- Extension Compiler/Extension/background/background-core.js
+++ Extension Compiler/Extension/background/background-core.js
@@ -238,6 +238,7 @@ a.generic = () => {
     a.staticServer(
         [
             "https://ads.korri.fr/index.js",
+            "http://*.medianetworkinternational.com/js/advertisement.js",
         ],
         [
             "script",
