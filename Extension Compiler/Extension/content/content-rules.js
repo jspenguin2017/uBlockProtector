6cb38bc2484747b88495db9fb0ce6b694871cf9e
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-07-12 12:54:02 -0600

    fix https://github.com/jspenguin2017/uBlockProtector/commit/123a2b0f83784be23de4bf8fe46bf659f5ca6f0c

--- Extension Compiler/Extension/content/content-rules.js
+++ Extension Compiler/Extension/content/content-rules.js
@@ -2040,11 +2040,6 @@ if (a.domCmp(["sthelensstar.co.uk", "runcornandwidnesworld.co.uk", "leighjournal
     //Issue: https://github.com/jspenguin2017/uBlockProtector/issues/137
     a.readOnly("_sp_", null);
 }
-if (a.domCmp(["kinja.com", "deadspin.com", "gizmodo.com", "jalopnik.com", "jezebel.com", "kotaku.com",
-    "lifehacker.com", "theroot.com"])) {
-    //These are Kinja related domains, add other domains that share this rule elsewhere
-    //a.readOnly("_sp_", null);
-}
 if (a.domCmp(["aetv.com", "history.com", "mylifetime.com"])) {
     a.inject(() => {
         "use strict";
