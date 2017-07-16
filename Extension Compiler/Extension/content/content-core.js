bfd5911328eca1dfdd1d4f826ceeb825c5b12281
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-07-15 20:34:05 -0600

    https://github.com/jspenguin2017/uBlockProtector/issues/362

--- Extension Compiler/Extension/content/content-core.js
+++ Extension Compiler/Extension/content/content-core.js
@@ -840,25 +840,23 @@ a.generic = () => {
             //Remove element
             insertedNode.remove();
         }
-        {
-            //NoAdBlock
-            if (insertedNode.nodeName === "CLOUDFLARE-APP" &&
-                insertedNode.getAttribute("app-id") === "no-adblock" &&
-                insertedNode.getAttribute(NoAdBlockMagic) !== "1") {
-                //Log
-                a.err("NoAdBlock");
-                //Remove element
-                insertedNode.remove();
-                //Create bait element
-                let bait = document.createElement("cloudflare-app");
-                bait.setAttribute("app-id", "no-adblock");
-                bait.setAttribute(NoAdBlockMagic, "1");
-                bait.style.display = "none";
-                document.documentElement.prepend(bait);
-                //Enable scrolling
-                $("body").rmClass("adbmodal-cloudflare-open");
-                //a.css("html, body { overflow:scroll; }");
-            }
+        //NoAdBlock
+        if (insertedNode.nodeName === "CLOUDFLARE-APP" &&
+            insertedNode.getAttribute("app-id") === "no-adblock" &&
+            insertedNode.getAttribute(NoAdBlockMagic) !== "1") {
+            //Log
+            a.err("NoAdBlock");
+            //Remove element
+            insertedNode.remove();
+            //Create bait element
+            let bait = document.createElement("cloudflare-app");
+            bait.setAttribute("app-id", "no-adblock");
+            bait.setAttribute(NoAdBlockMagic, "1");
+            bait.style.display = "none";
+            document.documentElement.prepend(bait);
+            //Enable scrolling
+            $("body").rmClass("adbmodal-cloudflare-open");
+            //a.css("html, body { overflow:scroll; }");
         }
         //StopAdblock
         if (insertedNode.nodeName === "DIV" &&
