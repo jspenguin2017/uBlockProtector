fddb84d59278273ebeedd4a769d315a54462c868
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-08-11 09:26:42 -0600

    #169

--- Extension Compiler/Extension/background/background-rules.js
+++ Extension Compiler/Extension/background/background-rules.js
@@ -141,3 +141,35 @@ a.generic();
         ],
     );
 }
+{
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        (details) => {
+            for (let i = 0; i < details.requestHeaders.length; i++) {
+                if (details.requestHeaders[i].name === "User-Agent") {
+                    details.requestHeaders.splice(i, 1);
+                    break;
+                }
+            }
+            return { requestHeaders: details.requestHeaders };
+        },
+        {
+            urls: [
+                "*://shorte.st/*",
+                "*://clkme.me/*",
+                "*://clkmein.com/*",
+                "*://cllkme.com/*",
+                "*://sh.st/*",
+                "*://viid.me/*",
+                "*://xiw34.com/*",
+            ],
+            types: [
+                "main_frame",
+                "sub_frame",
+            ],
+        },
+        [
+            "blocking",
+            "requestHeaders",
+        ],
+    );
+}
