afc5e7ecba6c8d95f5add56dcf35bb0ef7605776
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-08-31 07:35:11 -0600

    Fix #289

--- Extension Compiler/Extension/content/content-rules.js
+++ Extension Compiler/Extension/content/content-rules.js
@@ -104,7 +104,7 @@ if (a.domCmp(["apkmirror.com", "freepdf-books.com", "bc.vc", "themeslide.com", "
     "lewat.id", "cutwin.com", "cut-urls.com", "adbull.me", "xess.pro", "clik.pw", "admove.co",
     "adshort.co", "linksh.top", "adshorte.com", "coinb.ink", "123link.top", "fas.li", "zeiz.me",
     "u2s.io", "linclik.com", "xurl.us", "adbilty.me", "cpmlink.net", "gsurl.in", "linkhits.us",
-    "coshurl.co", "leechall.com", "gratisjuegos.co", "zonadescarga.info"])) {
+    "coshurl.co", "leechall.com", "gratisjuegos.co", "zonadescarga.info", "firstone.tv"])) {
     a.timewarp("setInterval", a.matchMethod.stringExact, "1000");
 }
 if (a.domCmp(["katfile.com", "goldescargas.com"])) {
