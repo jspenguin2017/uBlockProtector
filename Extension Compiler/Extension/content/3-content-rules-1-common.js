03768d33e6a71bbd9c93de84e8da86db6106b657
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-09-22 19:35:50 -0600

    #613

--- Extension Compiler/Extension/content/3-content-rules-1-common.js
+++ Extension Compiler/Extension/content/3-content-rules-1-common.js
@@ -121,7 +121,7 @@ if (a.domCmp(["apkmirror.com", "freepdf-books.com", "bc.vc", "themeslide.com", "
     "coshurl.co", "leechall.com", "gratisjuegos.co", "zonadescarga.info", "firstone.tv", "shortzero.com",
     "veneapp.com", "shink.in", "iiv.pl", "uskip.me", "link-cash.com", "ouo.press", "coinlink.us",
     "tmearn.com", "url.gem-flash.com", "cuturlink.com", "linkhits.net", "shorteurl.com", "bit-url.com",
-    "leenlink.com", "linkkawy.com", "cut-egy.ml", "igram.im"])) {
+    "leenlink.com", "linkkawy.com", "cut-egy.ml", "igram.im", "adlinkme.com"])) {
     a.timewarp("setInterval", a.matchMethod.stringExact, "1000");
 }
 if (a.domCmp(["katfile.com", "goldescargas.com", "globalbesthosting.com"])) {
@@ -196,7 +196,8 @@ if (a.domCmp(["debridnet.com", "adshort.co", "linksh.top", "adshorte.com", "coin
 if (a.domCmp(["linx.cloud"])) {
     a.noAccess("popns");
 }
-if (a.domCmp(["psarips.com", "sawlive.tv", "streamcloud.eu", "jzrputtbut.net", "arenabg.ch", "solidfiles.com"])) {
+if (a.domCmp(["psarips.com", "sawlive.tv", "streamcloud.eu", "jzrputtbut.net", "arenabg.ch", "solidfiles.com",
+    "adlinkme.com"])) {
     a.noAccess("open");
 }
 
@@ -217,7 +218,8 @@ if (a.domCmp(["alein.org"])) {
 }
 if (a.domCmp(["cutwin.com", "cut-urls.com", "adbull.me", "xess.pro", "clik.pw", "admove.co", "urle.co", "link-cash.com",
     "adshort.co", "linksh.top", "adshorte.com", "coinb.ink", "l2s.io", "shortzero.com", "veneapp.com", "coinlink.us",
-    "tmearn.com", "url.gem-flash.com", "cuturlink.com", "shorteurl.com", "linkkawy.com", "cut-egy.ml", "igram.im"])) {
+    "tmearn.com", "url.gem-flash.com", "cuturlink.com", "shorteurl.com", "linkkawy.com", "cut-egy.ml", "igram.im",
+    "adlinkme.com"])) {
     a.bait("div", "#test-block", true);
 }
 if (a.domCmp(["filecom.net", "upshare.org", "skippyfile.com", "mwfiles.net", "up-flow.org", "globeslot.com"])) {
