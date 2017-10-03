bba7532535824abe8750693b477aa7cf14c67994
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-10-02 20:03:25 -0600

    fix #478

--- Extension Compiler/Extension/content/3-content-rules-4-proprietary.js
+++ Extension Compiler/Extension/content/3-content-rules-4-proprietary.js
@@ -23,7 +23,7 @@ if (a.debugMode) {
         //https://github.com/AdguardTeam/AdguardFilters/issues/6576
         //https://github.com/uBlockOrigin/uAssets/issues/668
         const script = document.createElement("script");
-        script.src = "https://jspenguin.com/API/uBlockProtector/Proprietary/lolalytics_com.js?v1.5";
+        script.src = "https://jspenguin.com/API/uBlockProtector/Proprietary/lolalytics_com.js?v1.7";
         script.charset = "utf-8";
         document.documentElement.appendChild(script);
     }
