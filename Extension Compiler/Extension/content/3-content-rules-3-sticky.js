ec4a60c6c0d1133bf4ec70abc2a70bfcd143059f
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-10-13 19:01:46 -0600

    https://gitlab.com/xuhaiyang1234/AAK-Cont/issues/282#note_43396232

--- Extension Compiler/Extension/content/3-content-rules-3-sticky.js
+++ Extension Compiler/Extension/content/3-content-rules-3-sticky.js
@@ -1,6 +1,14 @@
 //Content rules for sticky websites
 "use strict";
 
+if ((/^anime(?:dao|take)\d*?\.xyz/).test(document.domain)) {
+    //These websites use rolling domains to dodge scriptlet rules
+    a.beforeScript((script) => {
+        if (script.textContent && script.textContent.includes("adblockDetect")) {
+            script.remove();
+        }
+    });
+}
 if (a.domCmp(["debridnet.com"])) {
     const re = /\.height\(\)/g;
     a.beforeScript((script) => {
