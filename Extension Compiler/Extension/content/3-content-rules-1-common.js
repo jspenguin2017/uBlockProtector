6fd833c88336946ad5094511d369d0012957ffa6
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-10-30 18:20:26 -0600

    https://gitlab.com/xuhaiyang1234/AAK-Cont/issues/325

--- Extension Compiler/Extension/content/3-content-rules-1-common.js
+++ Extension Compiler/Extension/content/3-content-rules-1-common.js
@@ -123,7 +123,8 @@ if (a.domCmp(["apkmirror.com", "freepdf-books.com", "bc.vc", "themeslide.com", "
     "tmearn.com", "url.gem-flash.com", "cuturlink.com", "linkhits.net", "shorteurl.com", "bit-url.com",
     "leenlink.com", "linkkawy.com", "cut-egy.ml", "igram.im", "adlinkme.com", "oke.io", "wolink.in",
     "bilink.xyz", "git.tc", "zlshorte.net", "shink.me", "oload.stream", "egy-links.com", "infinityurl.co",
-    "adsurl.xyz", "firstonetv.net", "lkcash.net", "123link.io", "curs.io", "psl.io"])) {
+    "adsurl.xyz", "firstonetv.net", "lkcash.net", "123link.io", "curs.io", "psl.io", "teqani-plus.com",
+    "link-money.com"])) {
     a.timewarp("setInterval", a.matchMethod.stringExact, "1000");
 }
 if (a.domCmp(["katfile.com", "goldescargas.com", "globalbesthosting.com", "userupload.net",
@@ -222,7 +223,7 @@ if (a.domCmp(["cutwin.com", "cut-urls.com", "adbull.me", "xess.pro", "clik.pw", 
     "adshort.co", "linksh.top", "adshorte.com", "coinb.ink", "l2s.io", "shortzero.com", "veneapp.com", "coinlink.us",
     "tmearn.com", "url.gem-flash.com", "cuturlink.com", "shorteurl.com", "linkkawy.com", "cut-egy.ml", "igram.im",
     "adlinkme.com", "ecut.io", "bit-url.com", "cut-earn.com", "adbilty.me", "egy-links.com", "infinityurl.co",
-    "adsurl.xyz", "lkcash.net", "123link.io", "curs.io", "psl.io"])) {
+    "adsurl.xyz", "lkcash.net", "123link.io", "curs.io", "psl.io", "teqani-plus.com"])) {
     a.bait("div", "#test-block", true);
 }
 if (a.domCmp(["filecom.net", "upshare.org", "skippyfile.com", "mwfiles.net", "up-flow.org", "globeslot.com"])) {
