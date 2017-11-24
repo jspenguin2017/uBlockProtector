c57b81d1d6f52d2e26ea6930132f5e22f45ea55a
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-11-23 17:29:24 -0700

    update doc

--- Extension Compiler/Extension/background/2-background-rules.js
+++ Extension Compiler/Extension/background/2-background-rules.js
@@ -206,6 +206,7 @@ a.generic();
 {
     //https://github.com/jspenguin2017/uBlockProtector/issues/398
     //https://gitlab.com/xuhaiyang1234/uBlockProtectorSecretIssues/issues/12
+    //https://github.com/AdguardTeam/AdguardFilters/issues/6718
     a.staticServer(
         [
             "*://ads-v-darwin.hulustream.com/published/*.mp4*",
