1f73f7cc8cf2aeeadfe94deedcd4e841e7eb88d6
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-01-04 16:19:51 -0700

    https://github.com/jspenguin2017/uBlockProtector/issues/785

--- Extension Compiler/Extension/content/3-content-rules-2-specific.js
+++ Extension Compiler/Extension/content/3-content-rules-2-specific.js
@@ -2252,6 +2252,9 @@ if (a.domCmp(["oddreaders.com"])) {
 if (a.domCmp(["onet.pl", "komputerswiat.pl", "przegladsportowy.pl"])) {
     a.beforeScript((script) => {
         if (script.id === "adsinit") {
+            if (a.isFirefox) {
+                script.textContent = "";
+            }
             script.remove();
         }
     });
