184d8e022432613061215a75a1bacda1b3860a06
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-04-30 13:27:44 -0600

    No commit message

---
+++ test/test-main.js
@@ -0,0 +1,23 @@
+/**
+ * Tests entry point.
+ */
+"use strict";
+
+
+/**
+ * Load modules.
+ * @const {Module}
+ */
+const assert = require("assert");
+const checkSyntax = require("./check-syntax.js");
+
+
+process.on("unhandledRejection", (e) => {
+    throw e;
+});
+
+assert(/[\\/]uBlockProtector$/.test(process.cwd()));
+
+(async () => {
+    await checkSyntax.validateDirectory(inputPath);
+})();
