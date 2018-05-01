6434409c38724cbe75e42d1df64e01af48a809ff
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-05-01 10:44:02 -0600

    14.1 rc

--- src/popup/index.js
+++ src/popup/index.js
@@ -3,3 +3,28 @@
  */
 "use strict";
 
+
+/**
+ * Home pages.
+ * @const {Map}
+ */
+const home = new Map([
+    ["0", ""],
+    ["1", "https://jspenguin2017.github.io/uBlockProtector/"],
+    ["2", "https://github.com/jspenguin2017/uBlockProtector"],
+]);
+
+/**
+ * On click handler.
+ * @function
+ * @listens click
+ */
+const onclick = function () {
+    const url = home.get(this.dataset.home) + this.dataset.href;
+    chrome.tabs.create({
+        url: url,
+    });
+};
+
+
+$(".wrapper").on("click", onclick);
