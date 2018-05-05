32beaaeeed20d575557f26703aef2c8376d452f6
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-05-04 23:14:34 -0600

    Finish first draft

--- src/reporter/index.js
+++ src/reporter/index.js
@@ -8,12 +8,37 @@
  * The key for local storage.
  * @const {string}
  */
-const storageKey = "quick-issue-reporter-last-report";
+const storageKeyLastReport = "quick-issue-reporter-last-report";
 /**
- * The time the user must wait between sending two messages.
+ * The time in milliseconds the user must wait between sending two messages.
  * @const {number}
  */
 const rateLimit = 900000;
+
+/**
+ * The number of characters the details field can contain.
+ * @const {number}
+ */
+const detailsLimit = 4096;
+/**
+ * Update the character count.
+ * @function
+ * @listens input
+ */
+const updateDetailsLimit = () => {
+    const length = $("#details").prop("value").length;
+
+    $("#character-count").text(
+        length.toString() + "/" + detailsLimit.toString(),
+    );
+
+    if (length > detailsLimit) {
+        $("#character-count").addClass("red");
+    } else {
+        $("#character-count").rmClass("red");
+    }
+};
+
 /**
  * The name of the host app.
  * @const {string}
@@ -21,8 +46,11 @@ const rateLimit = 900000;
 const appName = (() => {
     const manifest = chrome.runtime.getManifest();
     return manifest.name + " " + manifest.version;
-});
+})();
 
+
+$("#details").on("input", updateDetailsLimit);
+updateDetailsLimit();
 
 $("#send").on("click", async () => {
     const category = $("#category").prop("value");
@@ -39,8 +67,9 @@ $("#send").on("click", async () => {
         $("#msg-specific-error").addClass("open");
         return;
     }
-    if (details.length > 4096) {
-        $("#msg-specific-error p").text("Additional details can be at most 4096 characters long.");
+    if (details.length > detailsLimit) {
+        $("#msg-specific-error p").text("Additional details can be at most "
+            + detailsLimit.toString() + " characters long.");
         $("#msg-specific-error").addClass("open");
         return;
     }
@@ -62,7 +91,7 @@ $("#send").on("click", async () => {
     }
 
     if (response === "ok") {
-        localStorage.setItem(storageKey, Date.now());
+        localStorage.setItem(storageKeyLastReport, Date.now());
         $("#msg-report-sent").addClass("open");
     } else {
         console.error(response);
@@ -77,12 +106,25 @@ $("#msg-specific-error button").on("click", () => {
     $("#msg-specific-error").rmClass("open");
 });
 
-(() => {
-    const lastReport = localStorage.getItem(storageKey);
-    const now = Date.now();
-    if (typeof lastReport === "number" && lastReport + rateLimit > now) {
-        $("#msg-rate-limited").addClass("open");
+{
+    const init = () => {
+        const lastReport = localStorage.getItem(storageKeyLastReport);
+        const now = Date.now();
+        if (typeof lastReport === "number" && lastReport + rateLimit > now) {
+            $("#msg-rate-limited").addClass("open");
+        } else {
+            $("#main").rmClass("hidden");
+        }
+    };
+
+    if (/^\?\d+$/.test(location.search)) {
+        chrome.tabs.get(parseInt(location.search.substring(1)), (tab) => {
+            if (!chrome.runtime.lastError) {
+                $("#url").prop("value", tab.url);
+            }
+            init();
+        });
     } else {
-        $("#main").rmClass("hidden");
+        init();
     }
-})();
+}
