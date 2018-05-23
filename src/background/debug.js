5f4a723a667552b8d903f5d7c73d0cdf74370cf7
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-05-22 21:45:48 -0600

    https://github.com/uBlockOrigin/uAssets/issues/794

--- src/background/debug.js
+++ src/background/debug.js
@@ -46,7 +46,7 @@ if (a.debugMode) {
         // uplynk.com
         // https://github.com/uBlockOrigin/uAssets/issues/772
         const reBlock = /^https?:\/\/content[^.]*?\.uplynk\.com\/api\/v3\/preplay2\//;
-        const reStrip = /^https?:\/\/content[^.]*?\.uplynk\.com\/.*\.m3u8\?/;
+        const reStrip = /^https?:\/\/content[^.]*?\.uplynk\.com\/[^.?]*\.m3u8\?/;
 
         a.dynamicServer(
             [
