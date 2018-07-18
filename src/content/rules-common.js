bb2fd3efebe297b4dd76ecaa332a44b1f5f2d9a8
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-07-17 18:16:38 -0600

    fix false positive

--- src/content/rules-common.js
+++ src/content/rules-common.js
@@ -47,7 +47,7 @@
         "viafree.se", "sport-tv-guide.live", "anandabazar.com", "o2.pl",
         "vod.pl", "viasatsport.se", "viasport.fi", "tv3sport.dk",
         "viasport.no", "strefadb.pl", "wp.pl", "ostrzeszowinfo.pl",
-        "infostrow.pl", "lcpdfr.com", "dallasnews.com",
+        "infostrow.pl", "lcpdfr.com", "dallasnews.com", "rmcmv.us",
     ];
     const genericWhitelist2 = [
         // Local network
