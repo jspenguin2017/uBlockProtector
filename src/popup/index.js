2add0ca8f95841b9f77458a61dc4c23c51982818
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-07-30 20:36:51 -0600

    rework popup

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,11 +1,33 @@
-/**
- * Script for popup panel.
- */
+/******************************************************************************
+
+    Nano Defender - An anti-adblock defuser
+    Copyright (C) 2018  Nano Defender contributors
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
+
+    Popup panel script.
+
+******************************************************************************/
+
 "use strict";
 
+/*****************************************************************************/
 
 /**
- * Home pages.
+ * Homepage links.
  * @const {Map}
  */
 const home = [
@@ -15,25 +37,30 @@ const home = [
     chrome.runtime.getURL("/reporter/index.html"),
 ];
 
-/**
- * On click handler.
- * @function
- * @listens click
- */
-const onclick = function () {
-    const url = home[this.dataset.home] + this.dataset.href;
-    chrome.tabs.create({ url: url });
-};
+/*****************************************************************************/
 
+{
+    const manifest = chrome.runtime.getManifest();
+    $(".title > p").text(manifest.name + " " + manifest.version);
+}
+
+/*****************************************************************************/
 
 chrome.tabs.query({
     active: true,
     currentWindow: true,
 }, (tabs) => {
-    if (chrome.runtime.lastError || tabs.length === 0) {
+    if (chrome.runtime.lastError || tabs.length === 0)
         return;
-    }
-    home[3] += "?" + tabs[0].id;
+
+    home[3] = home[3] + "?" + tabs[0].id;
 });
-$(".title > p").text("Nano Defender " + chrome.runtime.getManifest().version);
-$(".wrapper").on("click", onclick);
+
+/*****************************************************************************/
+
+$(".wrapper").on("click", function () {
+    const url = home[this.dataset.home] + this.dataset.href;
+    chrome.tabs.create({ url: url });
+});
+
+/*****************************************************************************/
