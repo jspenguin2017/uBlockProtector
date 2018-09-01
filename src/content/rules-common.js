6cd43795dd4a640d95ed23f43ca20ea7131e2c3c
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-08-31 19:35:37 -0600

    refactor common rules

--- src/content/rules-common.js
+++ src/content/rules-common.js
@@ -230,36 +230,116 @@
 
 // a.filter
 
-if (a.domCmp(["usapoliticstoday.com", "vidlox.tv", "exrapidleech.info",
-    "urle.co", "gsmarena.com", "darmowe-pornosy.pl", "salon.com",
-    "linx.cloud", "flashx.tv", "flashx.to"])) {
+if (a.domCmp([
+    "darmowe-pornosy.pl",
+    "exrapidleech.info",
+    "flashx.to",
+    "flashx.tv",
+    "gsmarena.com",
+    "linx.cloud",
+    "salon.com",
+    "urle.co",
+    "usapoliticstoday.com",
+    "vidlox.tv",
+])) {
     a.filter("eval");
 }
-if (a.domCmp(["sc2casts.com", "webqc.org", "cloudwebcopy.com"])) {
+
+if (a.domCmp([
+    "cloudwebcopy.com",
+    "sc2casts.com",
+    "webqc.org",
+])) {
     a.filter("setTimeout");
 }
-if (a.domCmp(["vidoza.net", "videowood.tv", "l2s.io", "adshort.co",
-    "linksh.top", "adshorte.com", "coinb.ink", "1movies.tv", "katfile.com",
-    "firstrow.co", "firstrows.ru", "firstrows.tv", "firstrows.org",
-    "firstrows.co", "atdhe.mx", "atdhe.li", "atdhe.al", "atdhe.me",
-    "atdhe.to", "firstrows.biz", "firstrowus.eu", "firstrow1us.eu",
-    "firstsrowsports.eu", "firstrowsportes.tv", "firstrowsportes.com",
-    "justfirstrowsports.com", "hahasport.me", "wiziwig.ru", "wiziwig.sx",
-    "wiziwig.to", "wiziwig.tv", "myp2p.biz", "myp2p.tv", "myp2p.la",
-    "myp2p.ec", "myp2p.eu", "myp2p.sx", "myp2p.ws", "myp2p.com", "atdhe.ru",
-    "atdhe.se", "atdhe.bz", "atdhe.top", "u2s.io", "linclik.com", "iiv.pl",
-    "linkkawy.com", "zlshorte.net", "ur.ly", "oload.stream", "sawlive.tv",
-    "arenavision.in", "urle.co", "oload.info", "shink.me", "doramasflv.net",
-    "backin.net", "adshort.im", "ddlfr.pw"])) {
+
+if (a.domCmp([
+    "1movies.tv",
+    "adshort.co",
+    "adshort.im",
+    "adshorte.com",
+    "arenavision.in",
+    "atdhe.al",
+    "atdhe.bz",
+    "atdhe.li",
+    "atdhe.me",
+    "atdhe.mx",
+    "atdhe.ru",
+    "atdhe.se",
+    "atdhe.to",
+    "atdhe.top",
+    "backin.net",
+    "coinb.ink",
+    "ddlfr.pw",
+    "doramasflv.net",
+    "firstrow.co",
+    "firstrow1us.eu",
+    "firstrows.biz",
+    "firstrows.co",
+    "firstrows.org",
+    "firstrows.ru",
+    "firstrows.tv",
+    "firstrowsportes.com",
+    "firstrowsportes.tv",
+    "firstrowus.eu",
+    "firstsrowsports.eu",
+    "hahasport.me",
+    "iiv.pl",
+    "justfirstrowsports.com",
+    "katfile.com",
+    "l2s.io",
+    "linclik.com",
+    "linkkawy.com",
+    "linksh.top",
+    "myp2p.biz",
+    "myp2p.com",
+    "myp2p.ec",
+    "myp2p.eu",
+    "myp2p.la",
+    "myp2p.sx",
+    "myp2p.tv",
+    "myp2p.ws",
+    "oload.info",
+    "oload.stream",
+    "sawlive.tv",
+    "shink.me",
+    "u2s.io",
+    "ur.ly",
+    "urle.co",
+    "videowood.tv",
+    "vidoza.net",
+    "wiziwig.ru",
+    "wiziwig.sx",
+    "wiziwig.to",
+    "wiziwig.tv",
+    "zlshorte.net",
+])) {
     a.filter("open");
 }
-if (a.domCmp(["drivearabia.com", "putlocker.com", "kooora.com", "kooora.com",
-    "doatoolsita.altervista.org", "sockshare.com", "free-movie-home.com",
-    "pc.online143.com", "str3amtv.co.nr", "str3amtv.co.nr",
-    "str3amtv.altervista.org", "str3am.altervista.org", "filecom.net",
-    "pipocas.tv", "generatupremium.biz", "mega-debrid.eu",
-    "premiumst0re.blogspot.com", "dl-protect.com", "newsinlevels.com",
-    "vipracing.biz", "businesstoday.in"])) {
+
+if (a.domCmp([
+    "businesstoday.in",
+    "dl-protect.com",
+    "doatoolsita.altervista.org",
+    "drivearabia.com",
+    "filecom.net",
+    "free-movie-home.com",
+    "generatupremium.biz",
+    "kooora.com",
+    "kooora.com",
+    "mega-debrid.eu",
+    "newsinlevels.com",
+    "pc.online143.com",
+    "pipocas.tv",
+    "premiumst0re.blogspot.com",
+    "putlocker.com",
+    "sockshare.com",
+    "str3am.altervista.org",
+    "str3amtv.altervista.org",
+    "str3amtv.co.nr",
+    "str3amtv.co.nr",
+    "vipracing.biz",
+])) {
     a.filter("alert");
 }
 
@@ -267,64 +347,156 @@ if (a.domCmp(["drivearabia.com", "putlocker.com", "kooora.com", "kooora.com",
 
 // a.readOnly
 
-if (a.domCmp(["jansatta.com", "financialexpress.com", "indianexpress.com",
-    "uskip.me", "srt.am"])) {
+if (a.domCmp([
+    "financialexpress.com",
+    "indianexpress.com",
+    "jansatta.com",
+    "srt.am",
+    "uskip.me",
+])) {
     a.readOnly("RunAds", true);
 }
-if (a.domCmp(["jagranjunction.com", "nekopoi.bid", "catcatyfaucet.xyz"])) {
+
+if (a.domCmp([
+    "catcatyfaucet.xyz",
+    "jagranjunction.com",
+    "nekopoi.bid",
+])) {
     a.readOnly("isAdsDisplayed", true);
 }
-if (a.domCmp(["swissadspaysfaucet.com", "swissadspaysethfaucet.com",
-    "stream.nbcsports.com"])) {
+
+if (a.domCmp([
+    "stream.nbcsports.com",
+    "swissadspaysethfaucet.com",
+    "swissadspaysfaucet.com",
+])) {
     a.readOnly("adBlockEnabled", false);
 }
-if (a.domCmp(["ratemyprofessors.com"])) {
+
+if (a.domCmp([
+    "ratemyprofessors.com",
+])) {
     a.readOnly("adBlocker", false);
 }
-if (a.domCmp(["link.tl"])) {
+
+if (a.domCmp([
+    "link.tl",
+])) {
     a.readOnly("adblocker", false);
 }
-if (a.domCmp(["megogo.net"])) {
+
+if (a.domCmp([
+    "megogo.net",
+])) {
     a.readOnly("adBlock", false);
 }
-if (a.domCmp(["openload.co", "openload.io", "openload.tv", "oload.stream",
-    "nekopoi.bid", "translatica.pl", "angrybirdsnest.com", "4tests.com",
-    "urlaubspartner.net", "freizeitpartnerweb.de", "8bbit.com",
-    "outdoorpartner.net", "alcodistillers.ru"])) {
+
+if (a.domCmp([
+    "4tests.com",
+    "8bbit.com",
+    "alcodistillers.ru",
+    "angrybirdsnest.com",
+    "freizeitpartnerweb.de",
+    "nekopoi.bid",
+    "oload.stream",
+    "openload.co",
+    "openload.io",
+    "openload.tv",
+    "outdoorpartner.net",
+    "translatica.pl",
+    "urlaubspartner.net",
+])) {
     a.readOnly("adblock", false);
 }
-if (a.domCmp(["mexashare.com", "hanime.tv", "mega-estrenos.com", "shortin.ga",
-    "popunderjs.com", "code.ptcong.com"])) {
+
+if (a.domCmp([
+    "code.ptcong.com",
+    "hanime.tv",
+    "mega-estrenos.com",
+    "mexashare.com",
+    "popunderjs.com",
+    "shortin.ga",
+])) {
     a.readOnly("BetterJsPop", "window.Object.freeze({})");
 }
-if (a.domCmp(["youwatch.to", "he2eini7ka.com"])) {
+
+if (a.domCmp([
+    "he2eini7ka.com",
+    "youwatch.to",
+])) {
     a.readOnly("jsPopunder", () => { });
 }
-if (a.domCmp(["youwatch.org", "chouhaa.info", "ahzahg6ohb.com", "youwatch.to",
-    "he2eini7ka.com", "exashare.com",
-    "chefti.info", "bojem3a.info", "ajihezo.info", "yahmaib3ai.com"])) {
+
+if (a.domCmp([
+    "ahzahg6ohb.com",
+    "ajihezo.info",
+    "bojem3a.info",
+    "chefti.info",
+    "chouhaa.info",
+    "exashare.com",
+    "he2eini7ka.com",
+    "yahmaib3ai.com",
+    "youwatch.org",
+    "youwatch.to",
+])) {
     a.readOnly("adsShowPopup1", 1);
 }
-if (a.domCmp(["game-debate.com", "scan-mx.com", "onepiece-mx.net",
-    "naruto-mx.net"])) {
+
+if (a.domCmp([
+    "game-debate.com",
+    "naruto-mx.net",
+    "onepiece-mx.net",
+    "scan-mx.com",
+])) {
     a.readOnly("ad_block_test", () => { });
 }
-if (a.domCmp(["freebitcoins.nx.tc", "getbitcoins.nx.tc"])) {
+
+if (a.domCmp([
+    "freebitcoins.nx.tc",
+    "getbitcoins.nx.tc",
+])) {
     a.readOnly("ad_block_test", () => false);
 }
-if (a.domCmp(["kissanime.io", "kisscartoon.es", "1movies.tv",
-    "xmovies8.es"])) {
+
+if (a.domCmp([
+    "1movies.tv",
+    "xmovies8.es",
+])) {
     a.readOnly("check_adblock", true);
 }
-if (a.domCmp(["fourchette-et-bikini.fr", "meteocity.com"])) {
+
+if (a.domCmp([
+    "fourchette-et-bikini.fr",
+    "meteocity.com",
+])) {
     a.readOnly("adProtect", 1);
 }
-if (a.domCmp(["vipleague.is", "vipleague.ws", "vipleague.tv", "vipleague.se",
-    "vipleague.me", "vipapp.me", "vipleague.mobi", "vipleague.co",
-    "vipleague.sx", "vipleague.ch", "vipbox.tv", "vipbox.co", "vipbox.biz",
-    "vipbox.sx", "vipbox.eu", "vipbox.so", "vipbox.nu", "vipboxsa.co",
-    "strikeout.co", "strikeout.me", "homerun.re", "vipboxtv.co",
-    "securenetsystems.net"])) {
+
+if (a.domCmp([
+    "homerun.re",
+    "securenetsystems.net",
+    "strikeout.co",
+    "strikeout.me",
+    "vipapp.me",
+    "vipbox.biz",
+    "vipbox.co",
+    "vipbox.eu",
+    "vipbox.nu",
+    "vipbox.so",
+    "vipbox.sx",
+    "vipbox.tv",
+    "vipboxsa.co",
+    "vipboxtv.co",
+    "vipleague.ch",
+    "vipleague.co",
+    "vipleague.is",
+    "vipleague.me",
+    "vipleague.mobi",
+    "vipleague.se",
+    "vipleague.sx",
+    "vipleague.tv",
+    "vipleague.ws",
+])) {
     a.readOnly("iExist", true);
 }
 
@@ -332,17 +504,35 @@ if (a.domCmp(["vipleague.is", "vipleague.ws", "vipleague.tv", "vipleague.se",
 
 // a.noAccess
 
-if (a.domCmp(["debridnet.com", "adshort.co", "linksh.top", "adshorte.com",
-    "coinb.ink", "animeforce.org",
-    "imgrock.info", "srt.am"])) {
+if (a.domCmp([
+    "adshort.co",
+    "adshorte.com",
+    "animeforce.org",
+    "coinb.ink",
+    "debridnet.com",
+    "imgrock.info",
+    "linksh.top",
+    "srt.am",
+])) {
     a.noAccess("_pop");
 }
-if (a.domCmp(["linx.cloud"])) {
+
+if (a.domCmp([
+    "linx.cloud",
+])) {
     a.noAccess("popns");
 }
-if (a.domCmp(["psarips.com", "streamcloud.eu", "jzrputtbut.net", "arenabg.ch",
-    "solidfiles.com", "adlinkme.com", "maango.info", "vidfile.net",
-    "torrentfunk.com"])) {
+if (a.domCmp([
+    "adlinkme.com",
+    "arenabg.ch",
+    "jzrputtbut.net",
+    "maango.info",
+    "psarips.com",
+    "solidfiles.com",
+    "streamcloud.eu",
+    "torrentfunk.com",
+    "vidfile.net",
+])) {
     a.noAccess("open");
 }
 
@@ -350,41 +540,98 @@ if (a.domCmp(["psarips.com", "streamcloud.eu", "jzrputtbut.net", "arenabg.ch",
 
 // a.bait
 
-if (a.domCmp(["primeshare.tv", "leveldown.fr"])) {
+if (a.domCmp([
+    "leveldown.fr",
+    "primeshare.tv",
+])) {
     a.bait("div", "#adblock");
 }
-if (a.domCmp(["720pmkv.com", "psarips.com"])) {
+
+if (a.domCmp([
+    "720pmkv.com",
+    "psarips.com",
+])) {
     a.bait("div", "#advert");
 }
-if (a.domCmp(["osoarcade.com", "d3brid4y0u.info", "fileice.net", "nosteam.ro",
-    "openrunner.com", "easybillets.com", "spox.fr", "yovoyages.com",
-    "tv3.co.nz", "freeallmusic.info", "putlocker.com", "sockshare.com",
-    "dramapassion.com", "yooclick.com", "online.ua", "tgo-tv.com",
-    "bitcoiner.net", "litecoiner.net", "dogecatch.website", "dashcatch.xyz",
-    "shortify.pw"])) {
+
+if (a.domCmp([
+    "bitcoiner.net",
+    "d3brid4y0u.info",
+    "dashcatch.xyz",
+    "dogecatch.website",
+    "dramapassion.com",
+    "easybillets.com",
+    "fileice.net",
+    "freeallmusic.info",
+    "litecoiner.net",
+    "nosteam.ro",
+    "online.ua",
+    "openrunner.com",
+    "osoarcade.com",
+    "putlocker.com",
+    "shortify.pw",
+    "sockshare.com",
+    "spox.fr",
+    "tgo-tv.com",
+    "tv3.co.nz",
+    "yooclick.com",
+    "yovoyages.com",
+])) {
     a.bait("div", "#tester");
 }
-if (a.domCmp(["alein.org"])) {
+
+if (a.domCmp([
+    "alein.org",
+])) {
     a.bait("div", "#tester", true);
 }
-if (a.domCmp(["filecom.net", "upshare.org", "skippyfile.com", "mwfiles.net",
-    "up-flow.org", "globeslot.com"])) {
+
+if (a.domCmp([
+    "filecom.net",
+    "globeslot.com",
+    "mwfiles.net",
+    "skippyfile.com",
+    "up-flow.org",
+    "upshare.org",
+])) {
     a.bait("div", "#add");
 }
-if (a.domCmp(["razercrypt.com", "satoshiempire.com", "oneadfaucet.com",
-    "bluesatoshi.com"])) {
+
+if (a.domCmp([
+    "bluesatoshi.com",
+    "oneadfaucet.com",
+    "razercrypt.com",
+    "satoshiempire.com",
+])) {
     a.bait("div", "#test");
 }
-if (a.domCmp(["bitcoinaliens.com", "door2windows.com"])) {
+
+if (a.domCmp([
+    "bitcoinaliens.com",
+    "door2windows.com",
+])) {
     a.bait("ins", ".adsbygoogle");
 }
-if (a.domCmp(["leaguesecretary.com", "teknogods.com", "hellsmedia.com"])) {
+
+if (a.domCmp([
+    "hellsmedia.com",
+    "leaguesecretary.com",
+    "teknogods.com",
+])) {
     a.bait("div", "#adpbtest");
 }
-if (a.domCmp(["freesportsbet.com", "sportsplays.com"])) {
+
+if (a.domCmp([
+    "freesportsbet.com",
+    "sportsplays.com",
+])) {
     a.bait("div", "#ad-tester");
 }
-if (a.domCmp(["bitcoiner.net", "litecoiner.net"])) {
+
+if (a.domCmp([
+    "bitcoiner.net",
+    "litecoiner.net",
+])) {
     a.bait("div", "#ad-top");
 }
 
