08a1569d46c3ebab4edf8e2e8b37b3b63e696681
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-12-07 19:11:36 -0700

    No commit message

--- src/content/core.js
+++ src/content/core.js
@@ -1081,7 +1081,7 @@ a.generic = () => {
             let isEvil = false;
             const anErr = new window.Error("[Nano] Generic Solution Triggered :: AdBlock Notify");
             window.Object.defineProperty(window, "anOptions", {
-                configurable: true,
+                configurable: true, // Important, must allow script snippets to override
                 set(arg) {
                     try {
                         if (arg instanceof window.Object &&
