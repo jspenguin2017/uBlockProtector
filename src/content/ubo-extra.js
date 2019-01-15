aa4474ffca5d317f6a602c84ebcd8124b4051641
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-01-14 18:50:07 -0700

    pull upstream update

--- src/content/ubo-extra.js
+++ src/content/ubo-extra.js
@@ -228,6 +228,7 @@ if ( !abort ) {
             'gamerevolution.com',
             'gamespot.com',
             'geek.com',
+            'goal.com',
             'gofugyourself.com',
             'growthspotter.com',
             'hearthhead.com',
