51c5de39d105406c6a46b41087cfd1258b0db8fd
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-01-16 18:59:46 -0700

    https://github.com/jspenguin2017/uBlockProtector/issues/1015 https://github.com/jspenguin2017/uBlockProtector/issues/1027

--- src/background/debug.js
+++ src/background/debug.js
@@ -66,6 +66,52 @@ if (a.debugMode) {
             },
         );
     }
+
+    {
+        // shorte.st and related domains
+        // https://github.com/jspenguin2017/uBlockProtector/issues/1015
+        chrome.webRequest.onBeforeSendHeaders.addListener(
+            (details) => {
+                for (const header of details.requestHeaders) {
+                    if (header.name === "User-Agent")
+                        header.value = "curl/7.47.0";
+                }
+                return { requestHeaders: details.requestHeaders };
+            },
+            {
+                urls: [
+                    "*://shorte.st/*",
+                    "*://5k4i.com/*",
+                    "*://ceesty.com/*",
+                    "*://clkme.me/*",
+                    "*://clkmein.com/*",
+                    "*://cllkme.com/*",
+                    "*://corneey.com/*",
+                    "*://destyy.com/*",
+                    "*://festyy.com/*",
+                    "*://gestyy.com/*",
+                    "*://pj45.com/*",
+                    "*://sh.st/*",
+                    "*://viid.me/*",
+                    "*://wiid.me/*",
+                    "*://xiw34.com/*",
+                    "*://iklan.master-cyber.com/*",
+                    "*://links.orgasmatrix.com/*",
+                    "*://wik34.com/*",
+                    "*://zryydi.com/*",
+                    "*://skiip.me/*",
+                ],
+                types: [
+                    "main_frame",
+                    "sub_frame",
+                ],
+            },
+            [
+                "blocking",
+                "requestHeaders",
+            ],
+        );
+    }
 }
 
 //@pragma-end-if
--- src/background/rules.js
+++ src/background/rules.js
@@ -7,48 +7,5 @@ a.init();
 a.generic();
 
 {
-    // shorte.st and related domains
-    // https://github.com/jspenguin2017/uBlockProtector/issues/169
-    // https://github.com/NanoMeow/QuickReports/issues/333
-    chrome.webRequest.onBeforeSendHeaders.addListener(
-        (details) => {
-            for (const header of details.requestHeaders) {
-                if (header.name === "User-Agent")
-                    header.value = "curl/7.47.0";
-            }
-            return { requestHeaders: details.requestHeaders };
-        },
-        {
-            urls: [
-                "*://shorte.st/*",
-                "*://5k4i.com/*",
-                "*://ceesty.com/*",
-                "*://clkme.me/*",
-                "*://clkmein.com/*",
-                "*://cllkme.com/*",
-                "*://corneey.com/*",
-                "*://destyy.com/*",
-                "*://festyy.com/*",
-                "*://gestyy.com/*",
-                "*://pj45.com/*",
-                "*://sh.st/*",
-                "*://viid.me/*",
-                "*://wiid.me/*",
-                "*://xiw34.com/*",
-                "*://iklan.master-cyber.com/*",
-                "*://links.orgasmatrix.com/*",
-                "*://wik34.com/*",
-                "*://zryydi.com/*",
-                "*://skiip.me/*",
-            ],
-            types: [
-                "main_frame",
-                "sub_frame",
-            ],
-        },
-        [
-            "blocking",
-            "requestHeaders",
-        ],
-    );
+    // No specific rules for now
 }
