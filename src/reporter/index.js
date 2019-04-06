27070cf3aec91327b457beb5d08f61e032e263a5
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-04-06 11:45:16 -0600

    draft refactor

--- src/reporter/index.js
+++ src/reporter/index.js
@@ -1,66 +1,31 @@
-/**
- * Script for quick issue reporter.
- */
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// Nano Defender - An anti-adblock defuser
+// Copyright (C) 2016-2019  Nano Defender contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// Script for quick issue reporter
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
 "use strict";
 
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * The key for local storage.
- * @const {string}
- */
-const storageKeyLastReport = "quick-issue-reporter-last-report";
-/**
- * The time in milliseconds the user must wait between sending two messages.
- * @const {number}
- */
-const rateLimit = 900000;
-
-/**
- * The number of characters the details field can contain.
- * @const {number}
- */
-const detailsLimit = 3072;
-/**
- * Update the character count.
- * @function
- * @listens input
- */
-const updateDetailsLimit = () => {
-    const length = $("#details").prop("value").length;
-
-    $("#character-count").text(
-        length.toString() + "/" + detailsLimit.toString(),
-    );
-
-    if (length > detailsLimit) {
-        $("#character-count").addClass("red");
-    } else {
-        $("#character-count").rmClass("red");
-    }
-};
-
-/**
- * The name and version of the host app.
- * @const {string}
- */
-const appName = (() => {
-    const manifest = chrome.runtime.getManifest();
-    return manifest.name + " " + manifest.version;
-})();
-/**
- * Show a specific error message.
- * @function
- * @param {DOMString} msg - The message to show
- */
-const showMessage = (msg) => {
-    $("#msg-specific-error p").html(msg);
-    $("#msg-specific-error").addClass("open");
-};
-
-/**
- * Domains that are known to be working as intended.
- * @const {Array.<string>}
- */
 const knownGood = [
     // Wrong page
     "addons.mozilla.org",
@@ -91,43 +56,73 @@ const knownGood = [
     // Other known good
     "youtube.com",
 ];
-/**
- * Domains that are known to be bad where only technical help is needed.
- * @const {Array.<string>}
- */
+
 const knownBad = [
     "kissanime.ru",
     "kissmanga.com",
     "maddertranslates.com",
     "twitch.tv",
 ];
-/**
- * Check if a domain matches one of the matchers.
- * @param {string} domain - The domain to test.
- * @param {Array.<string>} matchers - The matchers.
- * @return {boolean} True if the domain matches, false otherwise.
- */
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+let initialUrl = "";
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+const storageKeyLastReport = "quick-issue-reporter-last-report";
+const bugReportDomain = "nanobugreport.hugoxu.com";
+
+const rateLimit = 900000; // 15 minutes
+const detailsLimit = 3072;
+
+const updateDetailsLimit = () => {
+    const length = $("#details").prop("value").length;
+
+    $("#character-count").text(length.toString() + "/" + detailsLimit.toString());
+
+    if (length > detailsLimit)
+        $("#character-count").addClass("red");
+    else
+        $("#character-count").rmClass("red");
+};
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+const appName = (() => {
+    const manifest = chrome.runtime.getManifest();
+    return manifest.name + " " + manifest.version;
+})();
+
+const showMessage = (msg) => {
+    $("#msg-specific-error p").html(msg);
+    $("#msg-specific-error").addClass("open");
+};
+
 const domCmp = (domain, matchers) => {
+    if (domain === bugReportDomain)
+        return false;
+
     for (const d of matchers) {
-        if (
-            domain.endsWith(d) &&
-            (
-                domain.length === d.length ||
-                domain.charAt(domain.length - d.length - 1) === '.'
-            )
-        ) {
+        if (domain.endsWith(d) && (domain.length === d.length || domain.charAt(domain.length - d.length - 1) === "."))
             return true;
-        }
     }
     return false;
 };
 
+// ----------------------------------------------------------------------------------------------------------------- //
 
 $("#category").on("change", function () {
     if (this.value === "Ads") {
-        showMessage("For missed ads and popups, please try the " +
-            "<a href='https://forums.lanik.us/'>EasyList Forum</a> first.");
+        showMessage(
+            "For missed ads and popups, please try the <a href='https://forums.lanik.us/'>EasyList Forum</a> first.",
+        );
     }
+
+    if (this.value === "Bug")
+        $("#url").prop("value", "https://" + bugReportDomain + "/").attr("disabled", "");
+    else
+        $("#url").prop("value", initialUrl).rmAttr("disabled");
 });
 
 $("#details").on("input", updateDetailsLimit);
@@ -138,36 +133,28 @@ $("#send").on("click", async () => {
     const url = $("#url").prop("value").trim();
     const details = $("#details").prop("value");
 
-    if (!category) {
-        showMessage("Please select an issue type.");
-        return;
-    }
+    if (!category)
+        return void showMessage("Please select an issue type.");
 
     let domain = /^https?:\/\/([^/]+)/.exec(url);
-    if (!domain) {
-        showMessage("Please enter a valid URL.");
-        return;
-    }
+    if (!domain)
+        return void showMessage("Please enter a valid URL.");
+
     domain = domain[1];
-    if (domCmp(domain, knownGood)) {
-        $("#msg-known-good").addClass("open");
-        return;
-    }
-    if (domCmp(domain, knownBad)) {
-        $("#msg-known-bad").addClass("open");
-        return;
-    }
+    if (domCmp(domain, knownGood))
+        return void $("#msg-known-good").addClass("open");
+    if (domCmp(domain, knownBad))
+        return void $("#msg-known-bad").addClass("open");
 
     if (category === "Other" && details.length < 5) {
-        showMessage("Please add a quick explanation for the " +
-            "&quot;Other&quot; category that you have chosen.");
+        showMessage("Please add a quick explanation for the &quot;Other&quot; category that you have chosen.");
         return;
     }
-    if (details.length > detailsLimit) {
-        showMessage("Additional details can be at most " +
-            detailsLimit.toString() + " characters long.");
-        return;
-    }
+    if (category === "Bug" && details.length < 100)
+        return void showMessage("Please incude a detailed step-by-step reproduction guide of this bug.");
+
+    if (details.length > detailsLimit)
+        return void showMessage("Additional details can be at most " + detailsLimit.toString() + " characters long.");
 
     let response;
     try {
@@ -200,33 +187,34 @@ $(".popup-container button.float-right").on("click", function () {
     this.parentNode.parentNode.classList.remove("open");
 });
 
-{
-    const init = () => {
-        let lastReport = localStorage.getItem(storageKeyLastReport);
-        // Maximum accuracy of integer is about 16 digits
-        // Cap at 15 digits to be safe, which is still more than enough to
-        // represent the next 30 thousand years
-        if (/^\d{13,15}$/.test(lastReport)) {
-            lastReport = parseInt(lastReport);
-        }
+// ----------------------------------------------------------------------------------------------------------------- //
 
-        const now = Date.now();
-        if (typeof lastReport === "number" && lastReport + rateLimit > now) {
-            $("#msg-rate-limited").addClass("open");
-        } else {
-            $("#main").rmClass("hidden");
-        }
-    };
+const init = () => {
+    let lastReport = localStorage.getItem(storageKeyLastReport);
+    // Maximum accuracy of integer is about 16 digits
+    // Cap at 15 digits to be safe, which is still more than enough to represent the next 30 thousand years
+    if (/^\d{13,15}$/.test(lastReport))
+        lastReport = parseInt(lastReport);
 
-    if (/^\?\d{1,15}$/.test(location.search)) {
-        chrome.tabs.get(parseInt(location.search.substring(1)), (tab) => {
-            if (!chrome.runtime.lastError) {
+    const now = Date.now();
+    if (typeof lastReport === "number" && lastReport + rateLimit > now)
+        $("#msg-rate-limited").addClass("open");
+    else
+        $("#main").rmClass("hidden");
+};
+
+if (/^\?\d{1,15}$/.test(location.search)) {
+    chrome.tabs.get(parseInt(location.search.substring(1)), (tab) => {
+        if (!chrome.runtime.lastError) {
+            initialUrl = tab.url;
+            if ($("#url").prop("value").trim() === "")
                 $("#url").prop("value", tab.url);
-            }
+        }
 
-            init();
-        });
-    } else {
         init();
-    }
+    });
+} else {
+    init();
 }
+
+// ----------------------------------------------------------------------------------------------------------------- //
