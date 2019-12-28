2e82223db045f19a744d3a3fe909b0c436ba08bb
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-12-28 13:22:10 -0700

    reformat platform scripts

--- src/platform/edge-content.js
+++ src/platform/edge-content.js
@@ -1,7 +1,30 @@
-/**
- * Special content script for Edge.
- */
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// Nano Defender - An anti-adblock defuser
+// Copyright (C) 2016-2019  Nano Defender contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// Special content script for legacy Edge
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
 "use strict";
+
+// ----------------------------------------------------------------------------------------------------------------- //
 
 if (a.domCmp(["windowscentral.com"])) {
     a.noAccess("adonisHash");
@@ -11,3 +34,5 @@ if (a.domCmp(["windowscentral.com"])) {
         }
     });
 }
+
+// ----------------------------------------------------------------------------------------------------------------- //
